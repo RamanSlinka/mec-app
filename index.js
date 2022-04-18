198fc2e129171fe4418880d46879c537716df12d
RamanSlinka <romuch1986@gmail.com>
2022-04-18 15:29:14 +0200

    added routers and endpoints , added controllers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,13 +11,16 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-
+app.get('/', (req, res) => {
+    res.json('working')
+})
 
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
+        // await sequelize.authenticate()
+        // await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on  ${PORT} PORT`)
         )
     } catch (e) {
